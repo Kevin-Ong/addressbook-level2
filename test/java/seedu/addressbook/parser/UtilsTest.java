c3a9e6e697a5811f5cc4105fbb30c29c4605b20d
Kevin Ong <Kevin Ong>
2016-09-07 14:34:45 +0800

    T4A6

---
+++ test/java/seedu/addressbook/parser/UtilsTest.java
@@ -0,0 +1,18 @@
+package seedu.addressbook.parser;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.addressbook.common.Utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static seedu.addressbook.common.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.addressbook.common.Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX;
+
+public class UtilsTest {
+
+    private Utils utils;
+}
